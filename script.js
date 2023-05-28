cb40a9af114701b2f491d8e930e601118dc45c9c
Navya k <navikk99@gmail.com>
2023-05-28 23:40:51 +0530

    car

---
+++ script.js
@@ -0,0 +1,18 @@
+/*var audio =document.createElement('audio');
+audio.setAttribute('src', './sound.mp3');
+audio.loop=true;
+audio.play();*/
+
+/*var audio = document.createElement("audio");
+  audio.loop = true;
+  audio.src = "sound.mp3";
+  audio.type = "sound.mp3";*/
+  
+  /*var audio = document.createElement('audio');
+audio.setAttribute('src', 'sound.mp3'); 
+audio.loop=true;*/
+
+var audio = document.createElement('audio');
+audio.setAttribute('src', 'sound.mp3'); 
+audio.loop=true;
+audio.play();
